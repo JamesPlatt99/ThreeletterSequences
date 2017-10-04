5d6fbdebdbd89c42021a4399bec7cfdaa1feec43
James Platt <james.platt@neptune.com>
2017-10-04 14:34:01 +0100

    stores the frequency of each tls in a dictionary. Prints the 10 most frequent.

--- ThreeLetterSequences/ThreeLetterSequences/Program.cs
+++ ThreeLetterSequences/ThreeLetterSequences/Program.cs
@@ -12,15 +12,36 @@ namespace ThreeLetterSequences
     {
         static void Main(string[] args)
         {
+            String query = @"\w(?=\w\w)";
             string text = ReadFromFile();
+            text = text.ToLower();
+            //string text = "tra ttratra";
             int freqTRA = RegExApproach(text, "tra");
-            int freqTSL = RegExApproach(text, @"\w\w\w");
-            Dictionary<string, int> results = getDictionary(text,@"\w\w\w");
+            int freqTSL = RegExApproach(text, query);
+            Dictionary<string, int> results = getDictionary(text,query);
 
             Console.WriteLine("The string 'tra' appears {0} times, there are a total of {1} TLSs!",freqTRA,freqTSL);
             Console.WriteLine("There are a total of {0} unique TSLs.",results.Count);
-            Console.WriteLine("There are {0} unique TSLs that appear exactly {1} time(s).",getFreqFreq(results,freqTRA),freqTRA);
+            Console.WriteLine("There are {0} unique TLSs that appear exactly {1} time(s).",getFreqFreq(results,freqTRA),freqTRA);           
+
+            Console.WriteLine("The 10 most common TLSs are: ");
+            foreach(string curTSL in topTen(results))
+            {
+                Console.WriteLine("   " + curTSL);
+            }
             Console.ReadLine();
+        }
+
+        static string[] topTen(Dictionary<string, int> results)
+        {
+            string[] output = new string[10];
+            
+            for(int i = 0; i < 10; i++)
+            {
+                output[i] = results.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
+                results.Remove(output[i]);
+            }
+            return output;
         }
 
         static int getFreqFreq(Dictionary<string, int> results,int n)
@@ -39,25 +60,25 @@ namespace ThreeLetterSequences
         static int RegExApproach(string text,string query)
         {
             int count;
-            count = Regex.Matches(text.ToLower(), query).Count;
+            count = Regex.Matches(text, query).Count;
             return count;
-        }
-        
+        }        
 
         static Dictionary<string, int> getDictionary(string text, string query)
         {
             Dictionary<string, int> results = new Dictionary<string, int>();
-            MatchCollection matches = Regex.Matches(text.ToLower(), query);           
-
+            MatchCollection matches = Regex.Matches(text, query);
+            String curResVal;
             foreach (Match result in matches)
             {
-                if (results.ContainsKey(result.Value))
+                curResVal = text.Substring(result.Index, 3);
+                if (results.ContainsKey(curResVal))
                 {
-                    results[result.Value]++;                    
+                    results[curResVal]++;                    
                 }
                 else
                 {
-                    results.Add(result.Value, 1);
+                    results.Add(curResVal, 1);
                 }
             }
             return results;
